58775ee801bf49e684c3bb24e7cb0a6c9b81468e
eyedz9 <img.jgs@gmail.com>
2017-06-05 13:51:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ if (isset($_POST['submit']))
 			
 			move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $filename);
 			echo ("Upload and Conversion of " .$filename. " is complete.");
-			exec("/usr/bin/ffmpeg -i ".$uploadLocation.$filename." -r 25 -s ".$vidsize." ".$convertedLocation.$convname." 2>&1");
+			exec("/usr/bin/ffmpeg -i ".$uploadLocation.$filename." -c:v libx264 -crf 18 -r 25 -s ".$vidsize." ".$convertedLocation.$convname." 2>&1");
 			if (file_exists("converted/" . $convname))
 			{
 				if(unlink("upload/" . $filename)) echo '<br />'; echo ("Deleted the uploaded source file: " . $filename);
