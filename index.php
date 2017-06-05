5a0b8948dba9a1027bba65c1e7f18c140e0dd3a6
eyedz9 <img.jgs@gmail.com>
2017-06-05 10:57:19 -0700

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if (isset($_POST['submit']))
 	$convertedLocation = 'converted/';
 	$vidsize = $_POST['vidsize'];
 	$spacer = '_';
-	$convname = $vidsize, $spacer, $filename;
+	$convname = $vidsize . $spacer . $filename;
 		
 		if (in_array($file_ext,$allowed_file_types) && ($filesize < 20000000))
 	{	
@@ -37,7 +37,7 @@ if (isset($_POST['submit']))
 			if (file_exists("converted/" . $convname))
 			{
 				if(unlink("upload/" . $filename)) echo '<br />'; echo ("Deleted the uploaded source file: " . $filename);
-				echo '<br /><a href="converted/'$convname.'" target="_blank"> Download Video</a><br />';
+				echo '<br /><a href="converted/'.$convname.'" target="_blank"> Download Video</a><br />';
 			}			
 			
 		}
