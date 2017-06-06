c28f6fbc338cd49149fa749ccd746bad66980558
eyedz9 <img.jgs@gmail.com>
2017-06-05 20:34:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -39,9 +39,11 @@ if (isset($_POST['submit']))
 			
 			move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $filename);
 			exec("/usr/bin/ffmpeg -i ".$uploadLocation.$filename." -vcodec libx264 -b 900k -r 25 -s ".$vidsize." ".$convertedLocation.$convname." 2>&1");
+			exec("usr/bin/ffmpeg -ss 0.5 -i ".$convertedLocation.$convname.$vidext." -t 1 -s ".$vidsize." -f image2 imagefile.jpg 2>&1");
 			if (file_exists("converted/" . $convname))
 			{
 				if(unlink("upload/" . $filename)) echo '<br />'; echo ("Deleted the uploaded source file: " . $filename);
+				echo '<img src="imagefile.jpg" />'
 				echo '<br /><a href="converted/'.$convname.$vidext.'"> Download Pre-roll '.$convname.'</a><br />';
 			}
 			
